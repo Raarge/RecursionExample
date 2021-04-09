e0ea949f23e9bcc044780482127020a260251b7c
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2021-04-09 18:06:30 -0400

    Add project files.

---
+++ RecursionExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int f = factorial(4);
+            Console.WriteLine("Final value: " + f);
+        }
+
+        static int factorial(int n)
+        {
+            Console.WriteLine("The int n passed into factorial: " + n);
+            if (n == 1)
+                return 1;
+            else
+            {
+                int result = n * factorial(n - 1);
+                Console.WriteLine("Calling Factorial For: " + n + " which is: " + result);
+                return result;
+            }
+        }
+    }
+}
